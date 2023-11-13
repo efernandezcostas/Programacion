ff50dcdfb157037652199f540e9fcb7f0446e430
Enrique Fdez <efernandezcostas@danielcastelao.org>
2023-11-13 10:43:59 +0100

    añadido boletin 11

---
+++ Boletin11/Boletin11_02/src/boletin11_02/Boletin11_02.java
@@ -0,0 +1,14 @@
+
+package boletin11_02;
+
+public class Boletin11_02 {
+
+
+    public static void main(String[] args) {
+        
+        Juego obj1=new Juego();
+        obj1.crearJuego();
+
+    }
+    
+}
---
+++ Boletin11/Boletin11_02/src/boletin11_02/Juego.java
@@ -0,0 +1,34 @@
+
+package boletin11_02;
+
+import java.util.Scanner;
+
+
+public class Juego {
+    
+    Scanner sc = new Scanner (System.in);
+    
+    public void crearJuego(){
+        
+        int numSecreto=(int)(Math.random()*49+1);
+        int numj2;
+        
+        do{
+            System.out.print("Escribe un número entre 1 y 50: ");
+            numj2=sc.nextInt();
+            
+            if (numSecreto-numj2>20 || numj2-numSecreto>20){
+                System.out.println("Moi lonxe");
+            }else if(numSecreto-numj2>10 || numj2-numSecreto>10){
+                System.out.println("Lonxe");
+            }else if(numSecreto-numj2>5 || numj2-numSecreto>5){
+                System.out.println("Preto");
+            }else{
+                System.out.println("Moi preto");
+            }
+        }while(numj2!=numSecreto);
+        
+        System.out.println("Has ganado");
+    }
+    
+}
